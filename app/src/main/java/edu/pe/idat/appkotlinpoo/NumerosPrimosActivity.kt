b221b6d4fd8220c12c1e254aee51d9b8b498dcca
Diego Vicente <diegovic99@hotmail.com>
2022-04-29 20:54:04 -0500

    Ejercicio 1

---
+++ app/src/main/java/edu/pe/idat/appkotlinpoo/NumerosPrimosActivity.kt
@@ -0,0 +1,11 @@
+package edu.pe.idat.appkotlinpoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NumerosPrimosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numeros_primos)
+    }
+}
